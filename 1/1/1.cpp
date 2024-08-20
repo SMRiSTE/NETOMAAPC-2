f14be4027ad614e27a93254e2fc2f57d94eff4b5
SMRist <s.m.r.00800ist@gmail.com>
2024-08-21 01:01:37 +0300

    Initial commit

---
+++ 1/1/1.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include<thread>
+#include<chrono>
+#include<atomic>
+
+std::atomic<int> num = 1;
+
+void client(int n1) {
+    while (num < n1) {
+        num++;
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+
+    }
+}
+
+void Operator() {
+    while (num > 0) {
+        std::cout << "В очереди сейчас " << num << " человек" << std::endl;
+        num--;
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int max_cl = 10;
+    std::thread t1(client, max_cl);
+    std::thread t2(Operator);
+    t1.join();
+    t2.join();
+
+    return 0;
+}
---
+++ 2/2/2.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include<thread>
+#include<chrono>
+#include<string>
+#include<mutex>
+#include <cstdlib>
+#include<vector>
+
+std::mutex m;
+
+void ProgBar(int num_threads) {
+    int num = 12;
+    std::string Bar = "[            ]";
+    auto ST = std::chrono::steady_clock::now();
+    for (int i = 0; i < num; i++) {
+        std::unique_lock<std::mutex> lk(m);
+        Bar[i + 1] = '|';
+        std::cout << "Номер потока: " << num_threads << " / Id: " << std::this_thread::get_id() << " / Bar: " << Bar << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        lk.unlock();
+    }
+    auto FT = std::chrono::steady_clock::now();
+
+    auto Time = std::chrono::duration_cast<std::chrono::milliseconds>(FT - ST).count();
+    if (Bar[Bar.size() - 2] == '|') {
+        std::unique_lock<std::mutex> lk(m);
+        std::cout << "Номер потока: " << num_threads << " / Id: " << std::this_thread::get_id() << " / Bar: " << Bar << " / Time: " << Time <<std::endl;
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    std::vector<std::thread> threads;
+
+    for (int i = 1; i <= 5; ++i) {
+        threads.push_back(std::thread(ProgBar, i));
+    }
+
+    for (auto& t : threads) {
+        t.join();
+    }
+
+}
