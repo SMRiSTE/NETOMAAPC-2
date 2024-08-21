7b071f3472b7463252752b7c05f9699b672d3043
SMRist <s.m.r.00800ist@gmail.com>
2024-08-21 22:40:30 +0300

    Initial commit

--- 2/2/2.cpp
+++ 2/2/2.cpp
@@ -5,18 +5,27 @@
 #include<mutex>
 #include <cstdlib>
 #include<vector>
+#include"Timer.h"
 
 std::mutex m;
 
+int my_rand() {
+    int seed = 1;
+    seed = seed * 1103515245 + 12345;
+    return (seed / 65536) % 200;
+}
+
 void ProgBar(int num_threads) {
+    consol_parameter cp;
     int num = 12;
     std::string Bar = "[            ]";
     auto ST = std::chrono::steady_clock::now();
     for (int i = 0; i < num; i++) {
         std::unique_lock<std::mutex> lk(m);
         Bar[i + 1] = '|';
+        cp.SetPosition(0, num_threads - 1);
         std::cout << "Номер потока: " << num_threads << " / Id: " << std::this_thread::get_id() << " / Bar: " << Bar << std::endl;
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        std::this_thread::sleep_for(std::chrono::milliseconds(my_rand()));
         lk.unlock();
     }
     auto FT = std::chrono::steady_clock::now();
@@ -24,6 +33,7 @@ void ProgBar(int num_threads) {
     auto Time = std::chrono::duration_cast<std::chrono::milliseconds>(FT - ST).count();
     if (Bar[Bar.size() - 2] == '|') {
         std::unique_lock<std::mutex> lk(m);
+        cp.SetPosition(0, num_threads - 1);
         std::cout << "Номер потока: " << num_threads << " / Id: " << std::this_thread::get_id() << " / Bar: " << Bar << " / Time: " << Time <<std::endl;
     }
 }
---
+++ 3/3/3.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include<thread>
+#include<mutex>
+
+class Data {
+public:
+    int data;
+    std::mutex m;
+
+    Data(int d) : data(d) {}
+
+    int getData() {
+        return data;
+    }
+
+    void setData(int d) {
+        data = d;
+    }
+};
+
+void swap_w_l(Data& d1, Data& d2) {
+    std::lock(d1.m, d2.m);
+
+    int temp = d1.getData();
+    d1.setData(d2.getData());
+    d2.setData(temp);
+
+    d1.m.unlock();
+    d2.m.unlock();
+}
+
+void swap_w_sl(Data& d1, Data& d2) {
+    std::scoped_lock<std::mutex> sl{ d1.m, d2.m };
+
+    int temp = d1.getData();
+    d1.setData(d2.getData());
+    d2.setData(temp);
+}
+
+void swap_w_ul(Data& d1, Data& d2) {
+    std::unique_lock<std::mutex> ul1(d1.m);    
+    std::unique_lock<std::mutex> ul2(d2.m);
+
+    int temp = d1.getData();
+    d1.setData(d2.getData());
+    d2.setData(temp);
+}
+
+int main() {
+    Data d1(10);
+    Data d2(20);
+
+    swap_w_l(d1, d2);
+    std::cout << d1.getData() << "   " << d2.getData();
+}
